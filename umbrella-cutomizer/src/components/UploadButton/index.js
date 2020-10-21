1e37b34c268b6397c8ef41d3e7155b176efe7f13
Shahid Dhariwala <shahid.dhariwala@gmail.com>
2020-10-21 22:54:12 +0530

    Select File Images

--- umbrella-cutomizer/src/components/UploadButton/index.js
+++ umbrella-cutomizer/src/components/UploadButton/index.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Grid } from "@material-ui/core"
 import Styles from "./index.module.css";
 import BackupOutlinedIcon from '@material-ui/icons/BackupOutlined';
@@ -6,8 +6,32 @@ import CloseOutlinedIcon from '@material-ui/icons/CloseOutlined';
 
 const UploadButton = () => {
 
+    //State
+    const [fileSelected, setFileSelected] = useState();
+
     //Vairables
     const uploadLogoRef = useRef();
+
+    //Lifecycle
+
+    // Handler
+    const uploadHandler = () => {
+        uploadLogoRef.current.click();
+    }
+
+    const inputFileHandler = (e) => {
+        try {
+            setFileSelected(e.target.files[0]);
+        }
+        catch (e) {
+            console.error("Something Went Wrong , while selecting the file!!", e);
+        }
+    }
+
+    const removeSelectedFile = () =>
+    {
+        setFileSelected("");
+    }
 
     return (
         <div className={Styles.Button_Container}>
@@ -16,13 +40,13 @@ const UploadButton = () => {
                     <BackupOutlinedIcon className={`${Styles.Icon}`} />
                 </Grid>
                 <Grid item xs={10}>
-                    <input type="file" style={{ display: "none" }} ref={uploadLogoRef} />
-                    <div className={Styles.Button_Text}>
-                        Upload Logo
+                    <input type="file" style={{ display: "none" }} ref={uploadLogoRef} onChange={(e) => inputFileHandler(e)} />
+                    <div className={Styles.Button_Text} onClick={uploadHandler}>
+                        {fileSelected ? fileSelected.name : "Upload Logo"}
                     </div>
                 </Grid>
                 <Grid item xs={1}>
-                    <CloseOutlinedIcon className={`${Styles.Icon}`} />
+                    {fileSelected ? <CloseOutlinedIcon className={`${Styles.Icon}`} onClick={removeSelectedFile} /> : null}
                 </Grid>
             </Grid>
         </div>
